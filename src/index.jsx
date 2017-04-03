1686e33e7f02d507f69182a4d52444790e8c3726
Santiago Cabrera <scabfos@gmail.com>
2017-04-03 18:28:18 +0200

    first steps

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyA7Fgg5uoHmq1lcKbdBoJzleQr-kd8uBuc';
+
+const App = () => (<div>Hi!</div>);
+
+ReactDOM.render(<App />, document.querySelector('.container'));
