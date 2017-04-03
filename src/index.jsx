5a9cc51906686029e21a25a55d5c718082453943
Santiago Cabrera <scabfos@gmail.com>
2017-04-03 18:38:43 +0200

    first child

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyA7Fgg5uoHmq1lcKbdBoJzleQr-kd8uBuc';
 
-const App = () => (<div>Hi!</div>);
+const App = () => (
+  <div>
+    Hi!
+    <SearchBar />
+  </div>
+);
 
 ReactDOM.render(<App />, document.querySelector('.container'));
