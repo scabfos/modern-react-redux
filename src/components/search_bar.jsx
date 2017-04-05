9cc062e38345bbf740ec9ac690720d381e732067
Santiago Cabrera <scabfos@gmail.com>
2017-04-05 18:17:25 +0200

    passing callbacks

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => (<input />);
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Starting Value' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyA7Fgg5uoHmq1lcKbdBoJzleQr-kd8uBuc';
 
-const App = () => (
-  <div>
-    Hi!
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
